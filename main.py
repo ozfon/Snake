5c712fbfa71689da421fe82945d790c0ce9c4c93
Scott Gardner <scott.gardner94@gmail.com>
2021-12-24 01:06:33 -0700

    Updated the game to allow the snake to grow. The snake now grows if the fruit is collected. Added colors although it is too bright. Player loses if the snake touched itself. The body of the snake follows the head. Code needs comments.
    Todo - make the game look better now that mechanics seem to work.

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ Snake game
 
 # Import standard modules
 import pygame
-from settings import Player, Fruit, SCREEN_WIDTH, SCREEN_HEIGHT, background
+import numpy as np
+from settings import Player, Fruit, Body, SCREEN_WIDTH, SCREEN_HEIGHT, background
 
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
@@ -16,6 +17,42 @@ from pygame.locals import (
     QUIT,
     K_q,
 )
+
+
+def grab_location():
+    i = 0
+    for entity in snake_group:
+        loc_x[i] = entity.rect.center[0]
+        loc_y[i] = entity.rect.center[1]
+        i += 1
+    return loc_x, loc_y
+
+
+def grab_diff(loc_x, loc_y):
+    for i in range(len(snake_group.sprites())-1):
+        diff_x[i] = loc_x[i]-loc_x[i+1]
+        diff_y[i] = loc_y[i]-loc_y[i+1]
+    return diff_x, diff_y
+
+
+def update_position(entity):
+    entity.rect.move_ip(entity.u, entity.v)  #
+
+
+def update_direction(entity, x, y, i):  #
+    if diff_x[i] > 0 and diff_y[i] == 0:
+        entity.v = 0
+        entity.u = 5
+    elif diff_x[i] < 0 and diff_y[i] == 0:
+        entity.v = 0
+        entity.u = -5
+    elif diff_y[i] > 0 and diff_x[i] == 0:
+        entity.u = 0
+        entity.v = 5
+    elif diff_y[i] < 0 and diff_x[i] == 0:
+        entity.u = 0
+        entity.v = -5
+
 
 # Initialize pygame
 pygame.init()
@@ -35,13 +72,27 @@ RUNNING = True
 # Instantiate player
 player = Player()
 fruit = Fruit()
+body = Body(grid_size*4, SCREEN_HEIGHT//2)
 
 # Create groups to hold sprites
 fruit_group = pygame.sprite.Group()
+snake_group = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
+body_group = pygame.sprite.Group()
+snake_butt = pygame.sprite.Group()
+added_body = pygame.sprite.Group()
+
+snake_group.add(player)
+snake_group.add(body)
+body_group.add(body)
+body = Body(grid_size*3, SCREEN_HEIGHT//2)
+
+snake_group.add(body)
+body_group.add(body)
+snake_butt.add(body)
 fruit_group.add(fruit)
+all_sprites.add(snake_group)
 all_sprites.add(fruit_group)
-all_sprites.add(player)
 
 # Main loop
 while RUNNING:
@@ -58,7 +109,21 @@ while RUNNING:
         elif event.type == QUIT:
             RUNNING = False
 
+    loc_x = np.zeros(len(snake_group.sprites()))
+    loc_y = np.zeros(len(snake_group.sprites()))
+    diff_x = np.zeros(len(snake_group.sprites()) - 1)
+    diff_y = np.zeros(len(snake_group.sprites()) - 1)
+
     screen.fill((background))
+
+    loc_x, loc_y = grab_location()
+    diff_x, diff_y = grab_diff(loc_x, loc_y)
+
+    i = 0
+    for entity in body_group:
+        update_direction(entity, diff_x, diff_y, i)
+        update_position(entity)
+        i += 1
 
     pressed_keys = pygame.key.get_pressed()
     OUTSIDE = player.check_boundary()
@@ -72,8 +137,31 @@ while RUNNING:
 
     if pygame.sprite.spritecollideany(player, fruit_group):
         fruit.change_location()
+        for entity in snake_butt:
+            new_body_left = entity.rect.left
+            new_body_top = entity.rect.top
+            u_last_body = entity.u
+            v_last_body = entity.v
+            if entity.u > 0:
+                new_body_left -= 25
+            elif entity.u < 0:
+                new_body_left += 25
+            elif entity.v > 0:
+                new_body_top -= 25
+            elif entity.v < 0:
+                new_body_top += 25
+        snake_butt.remove(body)
+        body = Body(new_body_left, new_body_top)
+        body_group.add(body)
+        all_sprites.add(body)
+        snake_butt.add(body)
+        snake_group.add(body)
+        added_body.add(body)
 
-    # draw all sprites
+    if pygame.sprite.spritecollideany(player, added_body):
+        RUNNING = False
+
+# draw all sprites
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
     pygame.display.flip()
--- settings.py
+++ settings.py
@@ -7,54 +7,57 @@ from pygame.locals import (
 )
 import random
 
-# from pygame.locals import (
-#    K_UP,
-# )
-
+# screen properties
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+# declaring colors
 green = pygame.Color('#99ff66')
 red = pygame.Color('#ff6644')
 background = pygame.Color('#ffffff')
+blue = pygame.Color('#6666ff')
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.RUNNING = True
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
         self.size = 25
         self.surf = pygame.Surface((self.size, self.size))
-        self.surf.fill((green))
+        self.surf.fill(pygame.Color(self.r, self.g, self.b))
         self.rect = self.surf.get_rect()
-        self.rect.move_ip(SCREEN_HEIGHT//2, SCREEN_WIDTH//2)
-        self.u = 5
+        self.rect.move_ip(self.size*5, SCREEN_HEIGHT//2)  # set starting location
+        self.u = 5  # initially moving to the right
         self.v = 0
 
     def update_position(self, pressed_keys):
-        self.rect.move_ip(self.u, self.v)
+        self.rect.move_ip(self.u, self.v)  # gives the player movement
 
-    def update_direction(self, pressed_keys):
-        if pressed_keys[K_UP]:
+    def update_direction(self, pressed_keys):  # logic to control snake
+        if pressed_keys[K_UP] and self.v != 5:
             self.v = -5
             self.u = 0
-        if pressed_keys[K_DOWN]:
+        if pressed_keys[K_DOWN] and self.v != -5:
             self.v = 5
             self.u = 0
-        if pressed_keys[K_LEFT]:
+        if pressed_keys[K_LEFT] and self.u != 5:
             self.u = -5
             self.v = 0
-        if pressed_keys[K_RIGHT]:
+        if pressed_keys[K_RIGHT] and self.u != -5:
             self.u = 5
             self.v = 0
 
-    def check_boundary(self):
+    def check_boundary(self):  # check if snake has hit boundary
         if self.rect.left < 0:
             self.RUNNING = False
             return self.RUNNING
         if self.rect.right > SCREEN_WIDTH:
             self.RUNNING = False
             return self.RUNNING
-        if self.rect.top <= 0:
+        if self.rect.top < 0:
             self.RUNNING = False
             return self.RUNNING
         if self.rect.bottom > SCREEN_HEIGHT:
@@ -68,13 +71,36 @@ class Fruit(pygame.sprite.Sprite):
     def __init__(self):
         super(Fruit, self).__init__()
         self.size = 25
-        self.surf = pygame.Surface((self.size, self.size))
+        self.surf = pygame.Surface((self.size, self.size))  # make the surface a square
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
         self.surf.fill(background)
         self.rect = self.surf.get_rect()
         self.rect.move_ip(200, 200)
-        pygame.draw.circle(self.surf, red, (12.5, 12.5), 12.5)
+        pygame.draw.circle(self.surf, pygame.Color(self.r, self.g, self.b), (12.5, 12.5), 12.5)  # draw a circle on the square
 
     def change_location(self):
-        self.current_location = self.surf.get_rect()
+        self.current_location = self.surf.get_rect()  # move the fruit when eaten
         self.rect.left = random.randrange(0, 800, 25)
         self.rect.top = random.randrange(0, 600, 25)
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
+        pygame.draw.circle(self.surf, pygame.Color(self.r, self.g, self.b), (12.5, 12.5), 12.5)  # draw a circle on the square
+
+
+class Body(pygame.sprite.Sprite):
+    def __init__(self, starting_left_loc, starting_top_loc):
+        super(Body, self).__init__()
+        self.size = 25
+        self.surf = pygame.Surface((self.size, self.size))
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
+        self.surf.fill(pygame.Color(self.r, self.g, self.b))
+        self.rect = self.surf.get_rect()
+        self.rect.left = starting_left_loc
+        self.rect.top = starting_top_loc
+        self.u = 0
+        self.v = 0
